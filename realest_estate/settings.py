f0a32de39d86d627a77a53c9c6710470d10d60fd
MIKKE-WANGO <71769643+MIKKE-WANGO@users.noreply.github.com>
2022-04-03 22:22:47 +0300

    host

--- realest_estate/settings.py
+++ realest_estate/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-!)7x4@!d0b2a30zeyh*26vku4nkn0lk%=#*4_0@^10@b8=ncxb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['realest-estate-wango.herokuapp.com' ]
 
 
 # Application definition
