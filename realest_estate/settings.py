c2c6937bb217562504304a7fd510e7a5f35be9fe
MIKKE-WANGO <71769643+MIKKE-WANGO@users.noreply.github.com>
2022-04-03 21:51:10 +0300

    procfile

--- realest_estate/settings.py
+++ realest_estate/settings.py
@@ -96,6 +96,10 @@ DATABASES = {
         'HOST': 'localhost'
     }
 }
+#use db provided by heroku
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 #setting up email
